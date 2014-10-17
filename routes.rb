09eed6fd691f3bfb1d21eb23151fb450c4cfb814
calvached <calvache.diana@gmail.com>
2014-10-17 10:27:45 -0500

    Working on maintaining state with sessions

--- routes.rb
+++ routes.rb
@@ -1,46 +1,55 @@
+$: << File.expand_path(File.dirname(__FILE__)) + '/lib'
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+
 require 'sinatra'
 require 'json'
 require_relative 'lib/game'
 
-use Rack::Session::Pool
-#enable :sessions
+configure do
+  use Rack::Session::Pool
+  #enable :sessions
+  #set :session_secret, "My session secret"
+end
 
 before do
   content_type :json
   headers 'Access-Control-Allow-Origin' => '*',
           'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
 
-  game = Game.new
-  game.setup
-  session['game'] = game
+  create_game
 end
 
-get '/current_board' do
-  #require 'pry'
-  #binding.pry
-  session['game'].current_board.to_json
-end
+#class App < Sinatra::Application
+  get '/current_board' do
+    p '============================= CURRENT BOARD =================================='
+    p session
 
-post '/player_move' do
-  #require 'pry'
-  #binding.pry
+    session['game'].current_board.to_json
+  end
 
-  feedback = session['game'].get_feedback(params['move'])
-  session['game'].update_board(feedback, params['move'])
-  #binding.pry
+  post '/player_move' do
+    feedback = session['game'].get_feedback(params['move'])
+    session['game'].update_board(feedback, params['move'])
 
-  { moveStatus: feedback, announcement: 'You are warm' }.to_json
+    { moveStatus: feedback, announcement: 'You are warm' }.to_json
 
-  #session['game'].process_player_move(params['move']).to_json
-end
+    #session['game'].process_player_move(params['move']).to_json
+  end
 
-post '/board_size' do
-  p '================================= BOARD SIZE ====================================='
-  p params
+  post '/board_size' do
+    create_game
 
-  { status: 'received!' }.to_json
-end
+    session['game'].current_board.to_json
+  end
 
-not_found do
-  halt 404, 'Route not created for this!!!'
-end
+  not_found do
+    halt 404, 'Route not created for this!!!'
+  end
+
+  def create_game
+    #params['board_size']
+    game = Game.new('6')
+    game.setup
+    session['game'] = game
+  end
+#end
