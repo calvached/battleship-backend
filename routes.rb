6511f23929fdd7683f2064975487ce53bc7da0a6
calvached <calvache.diana@gmail.com>
2014-10-21 13:52:46 -0500

    Holding state of a game using a GameKeeper

--- routes.rb
+++ routes.rb
@@ -1,9 +1,6 @@
-#$: << File.expand_path(File.dirname(__FILE__)) + '/lib'
-#Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
-
-require 'sinatra'
-require 'yaml'
 require 'json'
+require "sinatra"
+require_relative 'lib/game_keeper'
 require_relative 'lib/game'
 
 use Rack::Session::Pool
@@ -16,48 +13,20 @@ before do
 end
 
 get '/current_board' do
-  current_board.to_json
+  GameKeeper.current_board.to_json
 end
 
 post '/player_move' do
-  #parse/read the YAML file then write new info onto file
-  feedback = session['game'].get_feedback(params['move'])
-  session['game'].update_board(feedback, params['move'])
-
-  { moveStatus: feedback, announcement: 'You are warm' }.to_json
-
-  #session['game'].process_player_move(params['move']).to_json
+  # need to rework the logic
+  { moveStatus: 'hit', announcement: 'You are warm' }.to_json
 end
 
 post '/board_size' do
-  game = Game.new(params['board_size'])
-  game.setup
+  GameKeeper.current_game = Game.new(params['board_size'])
 
-  write(game)
-  current_board.to_json
+  GameKeeper.current_board.to_json
 end
 
 not_found do
   halt 404, 'Route not created for this!!!'
-end
-
-def current_board
-  read_game.first.current_board
-end
-
-def write(game)
-  File.open(file, "w") do |file|
-    file.write(YAML::dump(game))
-  end
-end
-
-def read_game
-  $/="\n\n"
-  File.open(file, "r").reduce([]) do |array, game|
-    array << YAML::load(game)
-  end
-end
-
-def file
-  "game.yaml"
 end
